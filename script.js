9921266780aa6a1423298946c2e7f4b27bb67e59
Tai3031 <tainakarine57@gmail.com>
2025-09-04 18:20:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+}
+
+function baixarPDF() {
+  alert("Para baixar o currículo, use Ctrl+P (ou Command+P no Mac) e selecione 'Salvar como PDF'.");
+}
